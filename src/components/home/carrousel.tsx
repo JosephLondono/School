7fa6c9027c8fefb9340d379f0aa67f8c5e6fcea5
JosephLondono <londonoj888@gmail.com>
2024-10-06 21:01:54 -0500

    Change Image Carrousel for show image event

--- src/components/home/carrousel.tsx
+++ src/components/home/carrousel.tsx
@@ -55,18 +55,28 @@ export function CarouselPlugin({ events }: { events: Event[] }) {
             </Card>
           </div>
         </CarouselItem>
-        {events.map(({ id, description, title }) => (
+        {events.map(({ id, description, title, url_image }) => (
           <CarouselItem key={id}>
             <div className="p-1">
               <Card>
                 <CardContent className="w-full max-h-[400px] flex aspect-square items-center justify-center p-6 relative">
-                  <Image
-                    src={`/images/slider/img-1.png`}
-                    fill={true}
-                    alt="Imagen Slider"
-                    priority={true}
-                    className="opacity-90 blur-[2px]"
-                  />
+                  {url_image ? (
+                    <Image
+                      src={url_image}
+                      fill={true}
+                      alt="Imagen Slider"
+                      priority={true}
+                      className="object-cover"
+                    />
+                  ) : (
+                    <Image
+                      src={`/images/slider/imagen-principal.avif`}
+                      fill={true}
+                      alt="Imagen Slider"
+                      priority={true}
+                      className="object-cover"
+                    />
+                  )}
                   <div className="absolute bottom-4 left-4 flex flex-col bg-black/50 p-4 rounded-md  max-w-[90%]">
                     <span className="text-2xl sm:text-3xl md:text-4xl font-semibold z-10 text-[#0ced41]">
                       {title}
